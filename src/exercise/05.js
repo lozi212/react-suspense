654d5c618509d03fe97a46e553fab800d1157482
Loza Demeke <your-github-email@example.com>
2026-09-18 02:50:11 +0300

    Complete exercise 05

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -1,46 +1,36 @@
 // Suspense Image
 // http://localhost:3000/isolated/exercise/05.js
 
-import * as React from 'react'
+import React from 'react'
+
 import {
   fetchPokemon,
+  getImageUrlForPokemon,
   PokemonInfoFallback,
   PokemonForm,
-  PokemonDataView,
   PokemonErrorBoundary,
 } from '../pokemon'
+
 import {createResource} from '../utils'
 
+window.useRealAPI = true
+
+// --------------------------------------------------
+// Extra 2: Render as You Fetch
+// --------------------------------------------------
+
+const PokemonInfo = React.lazy(() =>
+  import('../lazy/pokemon-info-render-as-you-fetch'),
+)
+
 // ❗❗❗❗
-// 🦉 On this one, make sure that you UNCHECK the "Disable cache" checkbox
-// in your DevTools "Network Tab". We're relying on that cache for this
-// approach to work!
+// Make sure "Disable cache" is UNCHECKED
+// in the DevTools Network Tab.
 // ❗❗❗❗
 
-// we need to make a place to store the resources outside of render so
-// 🐨 create "cache" object here.
-
-// 🐨 create an Img component that renders a regular <img /> and accepts a src
-// prop and forwards on any remaining props.
-// 🐨 The first thing you do in this component is check whether your
-// imgSrcResourceCache already has a resource for the given src prop. If it does
-// not, then you need to create one (💰 using createResource).
-// 🐨 Once you have the resource, then render the <img />.
-// 💰 Here's what rendering the <img /> should look like:
-// <img src={imgSrcResource.read()} {...props} />
-
-function PokemonInfo({pokemonResource}) {
-  const pokemon = pokemonResource.read()
-  return (
-    <div>
-      <div className="pokemon-info__img-wrapper">
-        {/* 🐨 swap this img for your new Img component */}
-        <img src={pokemon.image} alt={pokemon.name} />
-      </div>
-      <PokemonDataView pokemon={pokemon} />
-    </div>
-  )
-}
+// --------------------------------------------------
+// Suspense configuration
+// --------------------------------------------------
 
 const SUSPENSE_CONFIG = {
   timeoutMs: 4000,
@@ -48,32 +38,75 @@ const SUSPENSE_CONFIG = {
   busyMinDurationMs: 700,
 }
 
+// --------------------------------------------------
+// Pokemon Resource Cache
+// --------------------------------------------------
+
 const pokemonResourceCache = {}
+
+// --------------------------------------------------
+// Extra 1: Avoid Waterfall
+// --------------------------------------------------
+
+function createPokemonInfoResource(pokemonName) {
+  const data = createResource(fetchPokemon(pokemonName))
+
+  const image = createResource(
+    preloadImage(getImageUrlForPokemon(pokemonName)),
+  )
+
+  return {
+    data,
+    image,
+  }
+}
 
 function getPokemonResource(name) {
   const lowerName = name.toLowerCase()
+
   let resource = pokemonResourceCache[lowerName]
+
   if (!resource) {
-    resource = createPokemonResource(lowerName)
+    resource = createPokemonInfoResource(lowerName)
     pokemonResourceCache[lowerName] = resource
   }
+
   return resource
 }
 
-function createPokemonResource(pokemonName) {
-  return createResource(fetchPokemon(pokemonName))
+// --------------------------------------------------
+// Preload Image
+// --------------------------------------------------
+
+function preloadImage(src) {
+  return new Promise(resolve => {
+    const img = document.createElement('img')
+
+    img.src = src
+
+    img.onload = () => resolve(src)
+  })
 }
+
+// --------------------------------------------------
+// App
+// --------------------------------------------------
 
 function App() {
   const [pokemonName, setPokemonName] = React.useState('')
-  const [startTransition, isPending] = React.useTransition(SUSPENSE_CONFIG)
-  const [pokemonResource, setPokemonResource] = React.useState(null)
+
+  const [startTransition, isPending] =
+    React.useTransition(SUSPENSE_CONFIG)
+
+  const [pokemonResource, setPokemonResource] =
+    React.useState(null)
 
   React.useEffect(() => {
     if (!pokemonName) {
       setPokemonResource(null)
       return
     }
+
     startTransition(() => {
       setPokemonResource(getPokemonResource(pokemonName))
     })
@@ -89,18 +122,31 @@ function App() {
 
   return (
     <div className="pokemon-info-app">
-      <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
+      <PokemonForm
+        pokemonName={pokemonName}
+        onSubmit={handleSubmit}
+      />
+
       <hr />
-      <div className={`pokemon-info ${isPending ? 'pokemon-loading' : ''}`}>
+
+      <div
+        className={`pokemon-info ${
+          isPending ? 'pokemon-loading' : ''
+        }`}
+      >
         {pokemonResource ? (
           <PokemonErrorBoundary
             onReset={handleReset}
             resetKeys={[pokemonResource]}
           >
             <React.Suspense
-              fallback={<PokemonInfoFallback name={pokemonName} />}
+              fallback={
+                <PokemonInfoFallback name={pokemonName} />
+              }
             >
-              <PokemonInfo pokemonResource={pokemonResource} />
+              <PokemonInfo
+                pokemonResource={pokemonResource}
+              />
             </React.Suspense>
           </PokemonErrorBoundary>
         ) : (
